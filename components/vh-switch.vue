18bee31f860a2918c40bbdb7d25dbe3a2a811fec
vhhgx <qieka_zhang@yeah.net>
2023-10-19 23:30:54 +0800

    feat 添加配置缓存

--- components/vh-switch.vue
+++ components/vh-switch.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex">
     <span>{{ title }}</span>
-    <vs-switch :model="switchs">
+    <vs-switch v-model="keys">
       <template #off>{{ status.off }}</template>
       <template #on>{{ status.on }}</template>
     </vs-switch>
@@ -11,13 +11,41 @@
 <script setup>
 const props = defineProps({
   title: String,
-  switchs: Boolean,
+  switchs: String,
   status: {
     type: Object,
     default: { on: '开启', off: '关闭' }
   }
 })
 
+// let keys = ref(useState(props.switchs).value)
+
+let keys = useState(props.switchs)
+
+// console.log('keeeeeeeys', keys)
+// onComputed()
+// let keys = computed(() => {
+//   // return
+//   // const status = props.switchs
+
+//   console.log('2222222', props.switchs)
+//   // console.log('3333333', useState(props.switchs).value)
+
+//   return useState(props.switchs).value
+
+//   // useState('langs')
+
+//   // return per.surname + '~' + per.name
+// })
+
+
+
+
+// const testC = () => {
+//   // console.log('这里绑定的', keys.value)
+useState(props.switchs, () => keys)
+//   // console.log('dddddd', useState(props.switchs).value)
+// }
 </script>
 
 <style></style>
---
+++ plugins/prefetch.js
@@ -0,0 +1,12 @@
+import * as config from '../compress.config'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook('app:created', async () => {
+    let data = config
+    let keys = Object.keys(data)
+
+    keys.forEach(async (item) => {
+      useState(item, () => config[item])
+    })
+  })
+})
