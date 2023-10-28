88438697955337c515491f94220e92da4f15283e
vhhgx <qieka_zhang@yeah.net>
2023-10-28 19:42:53 +0800

    feat 拆分fonts组件

---
+++ components/page/dialogs.vue
@@ -0,0 +1,38 @@
+<template>
+  <vs-dialog blur prevent-close v-model="isShowLongText" class="vh-dialog">
+    <template #header>
+      <div class="mt-2 text-base">{{ title }}</div>
+    </template>
+
+
+    <div class="con-form">
+      <slot></slot>
+    </div>
+
+    <template #footer>
+      <div class="footer-dialog">
+        <vs-button block @click="onConfirm">确定</vs-button>
+      </div>
+    </template>
+  </vs-dialog>
+</template>
+
+<script setup>
+const emit = defineEmits(['confirm'])
+const props = defineProps({
+  title: String
+})
+
+const onConfirm = () => {
+  emit('confirm')
+}
+</script>
+
+<style lang="sass">
+.vh-dialog
+  .footer-dialog
+    display: flex
+    align-items: center
+    justify-content: center
+    flex-direction: column
+</style>
---
+++ components/page/fonts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col">
+    <div class="flex items-center">
+      <div class="font-bold mr-2">{{ fontName }}</div>
+      <div v-for="item in icons">
+        <BaseTags :icon="item.icon || `text`">{{ item.text }}</BaseTags>
+      </div>
+    </div>
+    <div class="mt-6" :style="{ 'font-size': `${fontSize}px`, 'font-family': `${fontFamily}` }">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  fontName: String,
+  fontSize: Number,
+  fontFamily: String,
+  icons: Array
+})
+</script>
+
+<style></style>
